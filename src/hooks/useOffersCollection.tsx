a458f4466d587a8545f9eabab862289b257872a2
vapaus <snaqerou@gmail.com>
2021-04-24 21:32:21 +0200

    filter on offers collection

--- src/hooks/useOffersCollection.tsx
+++ src/hooks/useOffersCollection.tsx
@@ -1,6 +1,18 @@
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { firestore } from "App";
 
+type WhereFilterOperator =
+  | "<"
+  | "<="
+  | "=="
+  | "!="
+  | ">="
+  | ">"
+  | "array-contains"
+  | "in"
+  | "array-contains-any"
+  | "not-in";
+
 export type Offers = {
   workMethodology: string[];
   benefits: string[];
@@ -18,15 +30,35 @@ export type Offers = {
   salaryFrom: number;
   salaryTo: number;
   title: string;
+  seniority: string[];
+  employmentType: string[];
+};
+
+type Filter = {
+  key: keyof Offers;
+  operator: WhereFilterOperator;
+  value: any;
 };
 
 type OfferCollectionProps = {
   limit?: number;
+  orderBy?: keyof Offers;
+  filterBy?: Filter[];
 };
 
-const useOffersCollection = ({ limit = 5 }: OfferCollectionProps) => {
+const useOffersCollection = ({
+  limit = 5,
+  orderBy = "createdAt",
+  filterBy,
+}: OfferCollectionProps) => {
   const offersRef = firestore.collection("offers");
-  const query = offersRef.orderBy("createdAt").limit(limit);
+  let query = offersRef.orderBy(orderBy).limit(limit);
+
+  if (filterBy?.length) {
+    filterBy.forEach(({ key, operator, value }) => {
+      query = query.where(key, operator, value);
+    });
+  }
 
   return useCollectionData<Offers>(query, { idField: "id" });
 };
