7361ed416a6a8f7f5f7274876efc0f1445ff591a
vapaus <snaqerou@gmail.com>
2021-04-24 21:32:36 +0200

    offers page

--- src/pages/Offers/Offers.tsx
+++ src/pages/Offers/Offers.tsx
@@ -1,130 +1,150 @@
 import {
   Box,
+  Button,
   Container,
   Flex,
-  Text,
-  Button,
-  Input,
   Grid,
   GridItem,
-  Checkbox,
   NumberInput,
   NumberInputField,
-  Spacer,
   Select,
+  Spacer,
+  Text,
 } from "@chakra-ui/react";
+import CheckboxGroup from "components/CheckboxGroup/CheckboxGroup";
+import FilterSection from "components/FilterSection/FilterSection";
+import Input from "components/Input/Input";
 import OfferCard from "components/OfferCard/OfferCard";
-import React from "react";
+import { FormikProvider, useFormik } from "formik";
+import useOffersCollection from "hooks/useOffersCollection";
+import React, { useState } from "react";
 
 const seniority = ["Trainee", "Junior", "Mid", "Senior", "Expert"];
 
 const employmentType = ["B2B", "Permanent", "Mandate contract"];
 
-interface Props {}
+type Order = "createdAt" | "salaryFrom";
 
-const Offers = (props: Props) => {
+type OffersValues = {
+  seniority: string[];
+  salaryFrom: string;
+  salaryTo: string;
+  employmentType: string;
+  locations: string;
+  technologies: string[];
+};
+
+const Offers = () => {
+  const [sortBy, setSortBy] = useState<Order>("createdAt");
+  const formik = useFormik<OffersValues>({
+    initialValues: {
+      seniority: [],
+      salaryFrom: "",
+      salaryTo: "",
+      employmentType: "",
+      locations: "",
+      technologies: [],
+    },
+    validateOnChange: true,
+    onSubmit: () => {},
+  });
+
+  const [offers] = useOffersCollection({ limit: 10, orderBy: sortBy });
+
   return (
-    <Box w="100%" minH="1000" color="gray.700">
-      <Box
-        p="4"
-        d="flex"
-        alignItems="center"
-        flexDirection="column"
-        bgGradient="linear(to-l, #6be1ff, #367bfa)"
-        h="280"
-        justifyContent="center"
-        color="white"
-      >
-        <Text fontSize="4xl" fontWeight="bold" textAlign="center">
-          37 Offers for mobile developers
-        </Text>
-        <Text fontSize="xl">Find your dream job!</Text>
-      </Box>
-      <Container pt="4" maxW="container.lg" position="relative">
+    <FormikProvider value={formik}>
+      <Box w="100%" minH="1000" color="gray.700">
         <Box
           p="4"
           d="flex"
-          borderRadius="10"
-          bgColor="white"
-          boxShadow="xl"
-          height="85"
           alignItems="center"
-          position="absolute"
-          top="-40px"
-          left="15px"
-          right="15px"
+          flexDirection="column"
+          bgGradient="linear(to-l, #6be1ff, #367bfa)"
+          h="280"
+          justifyContent="center"
+          color="white"
         >
-          <Input placeholder="Search for job" />
-          <Button variant="solid" ml="4" colorScheme="blue">
-            Search
-          </Button>
+          <Text fontSize="4xl" fontWeight="bold" textAlign="center">
+            {offers?.length} Offers for mobile developers
+          </Text>
+          <Text fontSize="xl">Find your dream job!</Text>
         </Box>
-        <Grid templateColumns="repeat(7, 1fr)" gap={4}>
-          <GridItem colSpan={2} mt="16">
-            <Flex flexDirection="column" position="sticky" top="0">
-              <Text fontSize="md" fontWeight="500" mb="1" mt="3">
-                Seniority
-              </Text>
-              {seniority.map((item) => (
-                <Checkbox key={item}>{item}</Checkbox>
-              ))}
-              <Text fontSize="md" fontWeight="500" mb="1" mt="3">
-                Salary expectations
-              </Text>
-              <Flex justifyContent="center" alignItems="center">
-                <NumberInput defaultValue={0} mr="2">
-                  <NumberInputField />
-                </NumberInput>
-                -
-                <NumberInput defaultValue={30000} ml="2">
-                  <NumberInputField />
-                </NumberInput>
+        <Container pt="4" maxW="container.lg">
+          <Grid templateColumns="repeat(7, 1fr)" gap={4}>
+            <GridItem colSpan={2}>
+              <Flex flexDirection="column" position="sticky" top="0">
+                <FilterSection title="Seniority">
+                  <CheckboxGroup
+                    formik={formik}
+                    name="seniority"
+                    options={seniority}
+                  />
+                </FilterSection>
+                <FilterSection title="Salary expectations">
+                  <Flex justifyContent="center" alignItems="center">
+                    <NumberInput defaultValue={0} mr="2">
+                      <NumberInputField />
+                    </NumberInput>
+                    -
+                    <NumberInput defaultValue={30000} ml="2">
+                      <NumberInputField />
+                    </NumberInput>
+                  </Flex>
+                </FilterSection>
+                <FilterSection title="Employment Type">
+                  <CheckboxGroup
+                    formik={formik}
+                    name="employmentType"
+                    options={employmentType}
+                  />
+                </FilterSection>
+                <FilterSection title="Location">
+                  <Input
+                    name="location"
+                    formik={formik}
+                    placeholder="Location"
+                  />
+                </FilterSection>
+                <FilterSection title="Technologies"></FilterSection>
+                <Button variant="solid" mt="4" colorScheme="blue">
+                  Search
+                </Button>
               </Flex>
-              <Text fontSize="md" fontWeight="500" mb="1" mt="3">
-                Employment Type
-              </Text>
-              {employmentType.map((item) => (
-                <Checkbox key={item}>{item}</Checkbox>
-              ))}
-              <Text fontSize="md" fontWeight="500" mb="1" mt="3">
-                Location
-              </Text>
-              <Text fontSize="md" fontWeight="500" mb="1" mt="3">
-                Technologies
-              </Text>
-              <Button variant="solid" mt="4" colorScheme="blue">
-                Search
-              </Button>
-            </Flex>
-          </GridItem>
-          <GridItem colSpan={5} mt="16">
-            <Flex alignItems="center">
-              <Text fontSize="md" color="gray.400">
-                28 results
-              </Text>
-              <Spacer />
+            </GridItem>
+            <GridItem colSpan={5}>
               <Flex alignItems="center">
-                <Text fontSize="md" minWidth="16">
-                  Sort by
+                <Text fontSize="md" color="gray.400">
+                  {offers?.length} results
                 </Text>
-                <Select>
-                  <option value="price">Price</option>
-                  <option value="date">Date</option>
-                </Select>
+                <Spacer />
+                <Flex alignItems="center">
+                  <Text fontSize="md" minWidth="16">
+                    Sort by
+                  </Text>
+                  <Select
+                    value={sortBy}
+                    onChange={(e) => {
+                      setSortBy(e.target.value as Order);
+                    }}
+                  >
+                    <option value="salaryFrom">Price</option>
+                    <option value="createdAt">Date</option>
+                  </Select>
+                </Flex>
               </Flex>
-            </Flex>
-            <Flex flexDirection="column">
-              {[...Array(10)].map((_, index) => (
-                <OfferCard key={index} />
-              ))}
-            </Flex>
-            <Flex py="8" justifyContent="center">
-              pagination
-            </Flex>
-          </GridItem>
-        </Grid>
-      </Container>
-    </Box>
+              <Flex flexDirection="column">
+                {offers?.map((item) => (
+                  <OfferCard key={item.id} {...item} />
+                ))}
+              </Flex>
+              <Flex py="8" justifyContent="center">
+                pagination
+              </Flex>
+            </GridItem>
+          </Grid>
+        </Container>
+      </Box>
+    </FormikProvider>
   );
 };
 
